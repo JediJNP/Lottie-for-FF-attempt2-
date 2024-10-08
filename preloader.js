6c29aaea7be13f5638805baf17aaefb2c424f01e
JediJNP <157392595+JediJNP@users.noreply.github.com>
2024-10-08 22:26:12 +0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://jvsc99.github.io/preloaderJs/imagem.svg';
+svgImage.src = 'https://JediJNP.github.io/Lottie-for-FF-attempt2/DogAndCat.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
